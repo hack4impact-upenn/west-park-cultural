0d481706940070b8acb51d46cbaff7f64b0d6376
charlesjin123 <charlesjin123@gmail.com>
2024-02-04 16:21:09 -0500

    started form (made toggleable)

--- client/src/NewDonation/NewDonationPage.tsx
+++ client/src/NewDonation/NewDonationPage.tsx
@@ -1,27 +1,60 @@
 import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import { NavigateFunction, useNavigate } from 'react-router-dom';
-import { Typography, Grid } from '@mui/material';
+import { Typography, Grid, TextField } from '@mui/material';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import ScreenGrid from '../components/ScreenGrid';
+import FormGrid from '../components/form/FormGrid';
 
 /**
  The New Donation Page
  */
 function NewDonationPage() {
+  const [donationType, setDonationType] = React.useState<string | null>(
+    'donation',
+  );
+
+  const handleDonationType = (
+    event: React.MouseEvent<HTMLElement>,
+    newDonationType: string | null,
+  ) => {
+    setDonationType(newDonationType);
+  };
+
   return (
-    <ScreenGrid>
-      <Typography variant="h2">New Donation Form Page</Typography>
-      {/* <Grid item container justifyContent="center">
-        <PromoteButton
-          admin={admin}
-          handleSelfPromote={handleSelfPromote}
-          navigator={navigator}
+    <Grid container sx={{ m: 3 }} spacing={2}>
+      <Grid item>
+        <Typography variant="h2">Register New Donation</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <ToggleButtonGroup
+          value={donationType}
+          exclusive
+          onChange={handleDonationType}
+          aria-label="donation type"
+          size="large"
+        >
+          <ToggleButton value="donation" aria-label="donation">
+            Donation
+          </ToggleButton>
+          <ToggleButton value="sponsorship" aria-label="sponsorship">
+            Sponsorship
+          </ToggleButton>
+          <ToggleButton value="grant" aria-label="grant">
+            Grant
+          </ToggleButton>
+        </ToggleButtonGroup>
+      </Grid>
+      <Grid item>
+        <TextField
+          id="outlined-basic"
+          label="Donation Amount"
+          variant="outlined"
+          required
         />
       </Grid>
-      <Grid item container justifyContent="center">
-        <Button onClick={handleLogout}>Logout</Button>
-      </Grid> */}
-    </ScreenGrid>
+    </Grid>
   );
 }
 
